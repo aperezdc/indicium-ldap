6a34a83ec80190e8d7dce3f748dd39e8a53ad930
Adrian Perez de Castro <aperez@igalia.com>
2016-03-29 02:27:39 +0300

    Initial import

---
+++ .vim-template:.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © %YEAR% Igalia S.L.
+#
+# Distributed under terms of the GPLv3 or, at your option,
+# under the terms of the Apache 2.0 license.
+
+"""
+%HERE%
+"""
